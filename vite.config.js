0f71e9f43028e19e7ccbf786c98ecadbe1b3326a
RoanKnight123 <roanknight21@gmail.com>
2024-06-14 18:19:52 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 443,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'path/to/your/private.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'path/to/your/file.crt'))
+    }
+  }
+})
